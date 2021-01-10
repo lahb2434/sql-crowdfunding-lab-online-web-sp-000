b3dc0a0b0a78fb2fee90062e9042df1575e1475b
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-10 20:46:07 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,9 +41,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  ""
-end
-SELECT projects.category, CAST(SUM(pledges.amount) AS INT) FROM projects
+  "SELECT projects.category, CAST(SUM(pledges.amount) AS INT) FROM projects
    JOIN pledges ON projects.id = pledges.project_id
-   WHERE projects.category = 'books'
-   GROUP BY projects.title
+   WHERE projects.category = 'books'"
+  # GROUP BY projects.title
+end
